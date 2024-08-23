07a504ba17042a18a8fd7c23a37f03d9b4e9b7f4
gyukiss55 <gs.kiss@gmail.com>
2024-08-23 13:46:00 +0200

    Drone data

---
+++ BallanceControlWinClient/DroneControlData.cpp
@@ -0,0 +1,8 @@
+// DroneControlData.cpp
+
+#include <stdint.h>
+
+#include "DroneControlData.h"
+
+DroneControlData currentControl;
+DroneControlData previousControl;
---
+++ BallanceControlWinClient/DroneSensorData.cpp
@@ -0,0 +1,10 @@
+// DroneSensorData.cpp
+#include <stdint.h>
+
+#include "DroneSensorData.h"
+
+DroneSensorData droneSensorDataCurrent;
+
+uint32_t droneSensorDataVectorIndex;
+
+DroneSensorData droneSensorDataVector[DroneSensorDataVectorSize];
